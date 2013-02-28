c9474af0d8a927dfedcaaed80c9d5f7d15ee2c95
Andrés Gasson <agasson@red-elvis.net>
2013-03-01 07:42:49 +1300

    added auroan and erc capabilities - still need to re-add alert

--- gas-pack/init.el
+++ gas-pack/init.el
@@ -3,17 +3,23 @@
 ;; Load bindings config
 (live-load-config-file "clean-mode-line.el")
 (live-load-config-file "gas-org.el")
-;(live-load-config-file "gas-mobileorg.el")
+;;(live-load-config-file "gas-mobileorg.el")
+(live-load-config-file "gas-irc.el")
+(live-load-config-file "gas-sauron.el")
 (live-load-config-file "gas-bindings.el")
 (live-load-config-file "gas-spell.el")
 
 ;; Load alerts
-(live-add-pack-lib "alert")
-(require 'alert)
+;(live-add-pack-lib "sauron")
+;(require 'sauron)
 
-(defvar alert-default-style 'growl)
-(alert "This is an debugalert" :title "My Alert" :category 'debug)
-(alert "This is a SERIOUS alert" :category 'critical )
+;; Load alerts
+;(live-add-pack-lib "alert")
+;(require 'alert)
+
+;(defvar alert-default-style 'growl)
+;(alert "This is an debugalert" :title "My Alert" :category 'debug)
+;(alert "This is a SERIOUS alert" :category 'critical )
 
 ;(load-file (concat (live-pack-lib-dir) "gandalf.el"))
 
