87546417e8ae6836f956e4ffb43e0fad08d26030
Andrew Gasson <andrew.gasson@dimensiondata.com>
2015-01-27 10:29:21 +1300

    deleted all old directories, aka gas-gtd-pack, solarized, orglue

--- gas-gtd-pack/info.el
+++
@@ -1,3 +0,0 @@
-(live-pack-name "gas-gtd-pack")
-(live-pack-version "1.1")
-(live-pack-description "Gases Org-mode Get Things Done (GTD) pack.")
--- gas-gtd-pack/init.el
+++
@@ -1,5 +0,0 @@
-;; Org-GTD pack init file
-;; Time-stamp: <2014-09-04 21:05:17 agasson>
-;;
-
-(live-load-config-file "gtd-bindings.el")
